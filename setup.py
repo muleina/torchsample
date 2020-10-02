e54c51426ce8fb21db8dc80da61a4954bfc6821b
Mulugeta W. Asres <muleina2000@gmail.com>
2020-10-02 16:02:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='torchsample',
       version='0.1.4',
       description='High-Level Training, Augmentation, and Sampling for Pytorch',
-      author='NC Cullen', 'Mulugeta W'
-      author_email='nickmarch31@yahoo.com','muleina2000@gmail.com'
+      author='NC Cullen'
+      author_email='nickmarch31@yahoo.com'
       packages=find_packages()
      )
